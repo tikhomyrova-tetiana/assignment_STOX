ac6cc7389bf7ab7690e94a0800acacac623176e6
tikhomyrova-tetiana <tihomirova.tania@gmail.com>
2022-08-08 11:54:07 +0200

    feat/scroll_menu

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Navbar from "./components/Navbar/Navbar";
+import ScrollMenu from "./components/ScrollMenu/ScrollMenu";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <ScrollMenu />
     </div>
   );
 }
---
+++ src/components/ScrollMenu/ScrollMenu.js
@@ -0,0 +1,52 @@
+import React from "react";
+import "./ScrollMenu.css";
+import { data } from "./imgData";
+
+export default function ScrollMenu() {
+  const slideLeft = () => {
+    let slider = document.getElementById("slider");
+    slider.scrollLeft = slider.scrollLeft - 500;
+  };
+  const slideRight = () => {
+    let slider = document.getElementById("slider");
+    slider.scrollLeft = slider.scrollLeft + 500;
+  };
+
+  return (
+    <div className="menu">
+      <button className="arrow-button" onClick={slideLeft}>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-6 w-6"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+          strokeWidth={2}
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M15 19l-7-7 7-7"
+          />
+        </svg>
+      </button>
+      <div id="slider" className="slider">
+        {data.map((item) => (
+          <img className="menu-item" src={item.img} alt="/" />
+        ))}
+      </div>
+      <button className="arrow-button" onClick={slideRight}>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-6 w-6"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+          stroke-width="2"
+        >
+          <path strokeLinecap="round" strokeLinejoin="round" d="M9 5l7 7-7 7" />
+        </svg>
+      </button>
+    </div>
+  );
+}
