2792bc229378948a20f6cb03765835414922dfc8
tikhomyrova-tetiana <tihomirova.tania@gmail.com>
2022-08-08 11:12:55 +0200

    feat/nav_bar/hamburger_button

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import Navbar from "./components/Navbar/Navbar";
+
 function App() {
-  return <div className="App">STOX ASSIGNMENT</div>;
+  return (
+    <div className="App">
+      <Navbar />
+    </div>
+  );
 }
 
 export default App;
