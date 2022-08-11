d7091c6b28706a92fa2076dad92b3e5298dc401b
tikhomyrova-tetiana <tihomirova.tania@gmail.com>
2022-08-11 19:36:33 +0200

    fix/accordion_styles

--- src/components/ScrollMenu/ScrollMenu.js
+++ src/components/ScrollMenu/ScrollMenu.js
@@ -1,57 +1,44 @@
-import React, { useRef } from "react";
+import React from "react";
 import "./ScrollMenu.css";
 import { data } from "./imgData";
 
 export default function ScrollMenu() {
-  // const sliderDiv = useRef();
-  // const slide = (amount) => {
-  //   sliderDiv.current.scrollLeft = sliderDiv.current.scrollLeft + amount;
-  // };
-
   return (
     <div className="scroll-menu">
-      {/* <button className="arrow-button" onClick={() => slide(-500)}>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="h-6 w-6"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-          strokeWidth={2}
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M15 19l-7-7 7-7"
-          />
-        </svg>
-      </button> */}
-      <div className="carousel">
-        <div className="swiper-container">
-          <div className="swiper-wrapper">
-            {data.map((item) => (
-              <div className="swiper-slide" key={item.id}>
-                <div>
-                  <img className="menu-item" src={item.img} alt={item.name} />
-                  <p>{item.name}</p>
-                </div>
-              </div>
-            ))}
+      <div id="slider" className="slider">
+        {data.map((item) => (
+          <div className="container" key={item.id}>
+            <img className="menu-item" src={item.img} alt={item.name} />
+            <p>{item.name}</p>
           </div>
-        </div>
+        ))}
       </div>
-      {/* <button className="arrow-button" onClick={() => slide(500)}>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="h-6 w-6"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-          strokeWidth="2"
-        >
-          <path strokeLinecap="round" strokeLinejoin="round" d="M9 5l7 7-7 7" />
-        </svg>
-      </button> */}
     </div>
+    // <div className="carousel">
+    //   <div className="swiper-container">
+    //     <div className="swiper-wrapper">
+    //       {data.map((item) => (
+    //         <div
+    //           className={
+    //             data[0]
+    //               ? "first-item swiper-slide"
+    //               : "item swiper-slide swiper-slide-active"
+    //           }
+    //           key={item.id}
+    //         >
+    //           <div>
+    //             <a href="/nl/" className="item-link">
+    //               <h2 className="item-name">{item.name}</h2>
+    //               <div className="picture-gradient"></div>
+    //               <div className="picture image-wrapper">
+    //                 <img className="picture" src={item.img} alt={item.name} />
+    //               </div>
+    //             </a>
+    //           </div>
+    //         </div>
+    //       ))}
+    //     </div>
+    //   </div>
+    // </div>
   );
 }
